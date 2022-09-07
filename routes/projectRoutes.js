9fe6b8d16ae9bf36d48d52e0560f919aad484225
jlbrod <109424471+jlbrod@users.noreply.github.com>
2022-09-07 20:56:48 +1000

    UPDATE:  Patch bug in routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,10 +2,12 @@ var express = require("express")
 var router = express.Router();
 let controller = require("../controller");
 
+//retrieve projects route to projectController
 router.get('/', (req, res) => {
     controller.projectController.retrieveProjects(req,res)
 })
 
+//create projects route to projectController
 router.post('/', (req, res) => {
     controller.projectController.createProjects(req,res)
 })
